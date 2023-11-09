54bb515ab1425636fcc6f8b02a6c1bc02dcd9a50
Martin Renteria <mrrenteria0159@gmail.com>
2023-11-08 23:21:33 -0700

    added five day forecast

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let apiResponse = document.getElementById('current-city')
 let searchForm = document.getElementById('search-form')
+let elpaso = document.getElementById('elpaso')
+let atlanta = document.getElementById('atlanta')
+let denver = document.getElementById('denver')
+let dallas = document.getElementById('dallas')
+let newyork = document.getElementById('newyork')
+let charlotte= document.getElementById('charlotte')
+let seattle = document.getElementById('seattle')
+
 
 function getGeo(city) {
     let geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid=2effc000f4251d05ffc2fed2de56922d'
@@ -41,6 +49,7 @@ fetch(requestUrl)
     let tempEl = document.createElement('h3').textContent = 'Temperature: '+ data.main.temp
     let wind = document.createElement('h3').textContent = ' Wind: ' + data.wind.speed
     let hum = document.createElement('h3').textContent = ' Humidity ' + data.main.humidity
+    
     weatherBox.append(tempEl)
     weatherBox.append(wind)
     weatherBox.append(hum)
@@ -48,21 +57,60 @@ fetch(requestUrl)
 }
 function getFive(city, lat, lon) {
     let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=2effc000f4251d05ffc2fed2de56922d`
-    
     fetch(requestUrl) 
     .then(function (response) {
         console.log(response)
         return response.json()
     })
     .then(function(data) {
-        let forecast = document.getElementById('card-one')
-        forecast.innerHTML = ''
+        let forecastOne = document.getElementById('card-one')
+        let forecastTwo = document.getElementById('card-two')
+        let forecast3 = document.getElementById('card-three')
+        let forecast4 = document.getElementById('card-four')
+        let forecast5 = document.getElementById('card-five')
+        console.log('forecast data', data)
+        forecastOne.innerHTML = ''
+        forecastTwo.innerHTML = ''
+        forecast3.innerHTML = ''
+        forecast4.innerHTML = ''
+        forecast5.innerHTML = ''
         console.log(data)
         let date = document.getElementsByClassName('date')
         date.textContent = city
-        let dateOne = document.createElement('li').textContent = ' Today ' + data.list[0]
+        let today = document.createElement('h3').textContent = 'Today'
+        let oneFeel = document.createElement('li').textContent = 'Temperature' + data.list[0].main.temp
+        let wind = document.createElement('li').textContent = 'Wind' + data.list[0].wind.speed
+        forecastOne.append(today)
+        forecastOne.append(oneFeel)
+        forecastOne.append(wind)
+        let tomorrow = document.createElement('h3').textContent = 'Tomorrow'
+        let temp2 = document.createElement('li').textContent = 'Temperature' + data.list[7].main.temp
+        let wind2 = document.createElement('li').textContent = 'Wind' + data.list[7].wind.speed
+        forecastTwo.append(tomorrow)
+        forecastTwo.append(temp2)
+        forecastTwo.append(wind2)
+        let three = document.createElement('h3').textContent = data.list[15].dt_txt
+        let temp3 = document.createElement('li').textContent = 'Temperature' + data.list[15].main.temp
+        let wind3 = document.createElement('li').textContent = 'Wind' + data.list[15].wind.speed
+        forecast3.append(three)
+        forecast3.append(temp3)
+        forecast3.append(wind3)
+        let four = document.createElement('h3').textContent = data.list[23].dt_txt
+        let temp4 = document.createElement('li').textContent = 'Temperature' + data.list[23].main.temp
+        let wind4 = document.createElement('li').textContent = 'Wind' + data.list[23].wind.speed
+        forecast4.append(four)
+        forecast4.append(temp4)
+        forecast4.append(wind4)
+        let five = document.createElement('h3').textContent = data.list[23].dt_txt
+        let temp5 = document.createElement('li').textContent = 'Temperature' + data.list[31].main.temp
+        let wind5 = document.createElement('li').textContent = 'Wind' + data.list[31].wind.speed
+        forecast5.append(five)
+        forecast5.append(temp5)
+        forecast5.append(wind5)
 
 
+
+ 
     })
 }
 
@@ -72,14 +120,10 @@ searchForm.addEventListener('submit', function(event){
     console.log(cityName)
     getGeo(cityName)
 })
+elpaso.addEventListener('click', function(event) {
+    let cityName = document.getElementById('elpaso').value
+    let geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q='+elpaso+'&limit=5&appid=2effc000f4251d05ffc2fed2de56922d'
+    console.log(cityName)
+    getGeo(geoCode)
+})
 
-
-// requestUrl = document.getElementById('submit')
-//    if (response.status === 200) {
-//     apiResponse.textContent = apiResponse
-//    }
-// On the submit button click, run geocode function to convert lat/long to city name
-// from city name, fetch api data
-// convert to json?
-// return to box id current-city
-// display different settings in divs within the box? humidity, temp, wind velocity etc?
